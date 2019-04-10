cd5bcc6873899543f358c5e3af64987e59acdbd7
唐祺峰 <tangqf@allcom.com>
2019-04-10 17:42:24 +0800

    jwt认证修改

--- tqf-admin/src/main/java/com/tqfframe/jwt/SecurityConfig.java
+++ tqf-admin/src/main/java/com/tqfframe/jwt/SecurityConfig.java
@@ -5,16 +5,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.List;
 
 /**
  *  一些理解：如果只配zuul的jwt认证，那么进入zuul网关的接口会进行认证。
  *  如果不走网关，直接写接口路径的话就会绕过zuul配置的jwt认证，所以必须每个需要认证的项目都要开启@EnableWebSecurity
- *
+ *  
  * Created by Tang-QiFeng on 2019/4/9
  */
 @EnableWebSecurity
@@ -26,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
+
     /**
      * 允许swagger相关的url通关
      */
@@ -39,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/swagger-resources/configuration/ui",
             "/swagge‌​r-ui.html"
     };
+
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
--- zuul-server/src/main/java/com/tqfframe/jwt/SecurityConfig.java
+++ zuul-server/src/main/java/com/tqfframe/jwt/SecurityConfig.java
@@ -18,6 +18,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import java.util.List;
 
 /**
+ *  一些理解：如果只配zuul的jwt认证，那么进入zuul网关的接口会进行认证。
+ *  如果不走网关，直接写接口路径的话就会绕过zuul配置的jwt认证，所以必须每个需要认证的项目都要开启@EnableWebSecurity
+ *
  * Created by Tang-QiFeng on 2019/4/9
  */
 @EnableWebSecurity
